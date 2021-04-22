a18295f1a9173a747e712a5e4c3cb4393d242c7e
Emilio Sanches <emilio.neto.32123@gmail.com>
2021-04-22 13:06:02 -0300

    Fixed syntax error on config.exs for migration configuration

--- config/config.exs
+++ config/config.exs
@@ -11,7 +11,7 @@ config :inmana,
   ecto_repos: [Inmana.Repo]
 
 config :inmana, Inmana.Repo,
-  migration_primary_key: [type: :binary_id]
+  migration_primary_key: [type: :binary_id],
   migration_foreign_key: [type: :binary_id]
 
 # Configures the endpoint
