85ac6e5e0233ec69eabfb777fb9b77e34299fae5
Emilio Sanches <emilio.neto.32123@gmail.com>
2021-04-22 15:59:09 -0300

    Added endpoint for showing supply by uuid

--- lib/inmana.ex
+++ lib/inmana.ex
@@ -9,7 +9,9 @@ defmodule Inmana do
 
   alias Inmana.Restaurants.Create, as: CreateRestaurant
   alias Inmana.Supplies.Create, as: CreateSupply
+  alias Inmana.Supplies.Get, as: GetSupply
 
   defdelegate create_restaurant(params), to: CreateRestaurant, as: :call
   defdelegate create_supply(params), to: CreateSupply, as: :call
+  defdelegate get_supply(id), to: GetSupply, as: :by_uuid
 end
