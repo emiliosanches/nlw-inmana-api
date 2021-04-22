ef749a0e530b81675e411bd36eb7ed2be9307ec9
Emilio Sanches <emilio.neto.32123@gmail.com>
2021-04-22 14:28:33 -0300

    Added FallbackController

--- lib/inmana_web/controllers/restaurants_controller.ex
+++ lib/inmana_web/controllers/restaurants_controller.ex
@@ -1,13 +1,17 @@
-defmodule Inmana.RestaurantsController do
+defmodule InmanaWeb.RestaurantsController do
   use InmanaWeb, :controller
 
   alias Inmana.Restaurant
   alias Inmana.Restaurants.Create
 
+  alias InmanaWeb.FallbackController
+
+  action_fallback FallbackController
+
   def create(conn, params) do
     with {:ok, %Restaurant{} = restaurant} <- Create.call(params) do
       conn
-      |> put_status(:ok)
+      |> put_status(:created)
       |> render("create.json", restaurant: restaurant)
     end
   end
